cad7a1f9b2b9ef0941bac4784c1a5baf9bd55191
Derek Vogt <derekvogt3@gmail.com>
2022-10-03 19:03:35 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col">
+      <div className="h-12 fixed border-4 border-black rounded shadow">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+      <div className="h-12"> </div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
